ea697d07911d55b20c12f76bf119994b516b40a9
Kylie <kylie8399@gmail.com>
2021-09-02 13:35:18 -0400

    Starting the about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>About</div>
+  <div>
+    <p>About</p>
+    <p1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
+      accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
+      id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
+      accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
+      id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
+      accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
+      id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
+    </p1>
+  </div>
 </template>
+
+<style scoped>
+p {
+  text-transform: uppercase;
+  padding-top: 30px;
+  font-size: 45px;
+}
+p {
+  padding: 30px;
+}
+</style>
