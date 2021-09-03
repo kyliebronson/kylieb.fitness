d19a482b144c902d44d6804f587cacdadc2204a5
Kylie <kylie8399@gmail.com>
2021-09-02 21:48:33 -0400

    added image to about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,20 @@
 <template>
-  <div>
-    <p>About</p>
-    <p1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
-      accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
-      id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
-      accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
-      id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
-      accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
-      id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
-    </p1>
-
-    <img src="../assets/me.JPG" />
+  <div class="main">
+    <div class="image"><img src="../assets/me.png" alt="" /></div>
+    <div class="paragraph">
+      <p>About</p>
+      <p1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
+        accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
+        id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
+        accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
+        id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deleniti,
+        accusamus dicta vero, aperiam quasi sunt incidunt nostrum error deserunt
+        id quod rem quisquam natus voluptas possimus nesciunt voluptatem aut.
+      </p1>
+    </div>
   </div>
 </template>
 
@@ -25,5 +26,24 @@ p {
 }
 p {
   padding: 30px;
+}
+.main {
+  position: relative;
+  width: 100%;
+  top: 20px;
+}
+.image {
+  position: absolute;
+  left: 20%;
+  border: 1px solid;
+}
+.paragraph {
+  position: absolute;
+  left: 42%;
+  border: 1px solid;
+  background-color: white;
+  padding: 18px;
+  width: 400px;
+  top: 60px;
 }
 </style>
